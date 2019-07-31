c5e25cd573a5a4aa2ad1786eb355b7ecdbe071c6
WiliTheSparrow <w.julia91@gmail.com>
2019-07-31 17:07:27 +0200

    changed get

--- index.js
+++ index.js
@@ -2,8 +2,13 @@ var express = require("express");
 var app = express();
 var port = process.env.PORT || 3000;
 
-app.get("/", function(req, res){
+/*app.get("/", function(req, res){
     res.send("Welcome to it GiGls Mobile Web App!");
+});*/
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname + '/public/index.html'));
 });
+
 
 app.listen(port);
