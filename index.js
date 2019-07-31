55969ce83d06b47e391804350f6e9b75062c9b70
WiliTheSparrow <w.julia91@gmail.com>
2019-07-31 16:26:50 +0200

    updated nodejs documents

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-//This specific port is indicated by the PORT environment variable. When your web server starts up on Heroku, make sure it’s listening on the port number specified by PORT:
-app.listen(process.env.PORT);
+var express = require("express");
+var app = express();
+var port = process.env.PORT || 3000;
 
-//To avoid having to set the PORT environment variable when running on your local machine, you can replace the line above with the following:
-let port = process.env.PORT;
-if (port == null || port == "") {
-  port = 5000;
-}
+app.get("/", function (req, res) {
+    res.send("Welcome to it GiGls Mobile Web App!");
+});
+
 app.listen(port);
