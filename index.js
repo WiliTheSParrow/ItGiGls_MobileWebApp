af4e9a5eb31a1c7cb3b6ea435c7fa7d5512346ea
WiliTheSparrow <w.julia91@gmail.com>
2019-07-31 16:50:09 +0200

    changed directory

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require("express");
 var app = express();
 var port = process.env.PORT || 3000;
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static('./public'));
 
 app.get("/", function (req, res) {
     response.render('index');
