0cb159948615008fba122c6d8986909b60d246d9
WiliTheSparrow <w.julia91@gmail.com>
2019-07-31 17:16:35 +0200

    pls work

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
+const path = require("path");
 var express = require("express");
 var app = express();
 var port = process.env.PORT || 3000;
 
-app.get("/", function(req, res){
-    res.send('../public/index.html');
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname + '/public/index.html'));
 });
 
 app.listen(port);
