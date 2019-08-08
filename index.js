c711e389ed48cc6a1bdfee54f05776ab20bedd55
WiliTheSparrow <w.julia91@gmail.com>
2019-08-08 12:07:45 +0200

    lets go back

--- index.js
+++ index.js
@@ -1,21 +1,12 @@
 const path = require("path");
-
 var express = require("express");
-
-var favicon = require('serve-favicon');
-
-var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
-
 var app = express();
-app.use(express.static(__dirname+''));
+var port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname + '/public/index.html'));
+});
+
 app.use(express.static(__dirname));
 
-app.use(express.static('src'));
-app.use(express('public'));
-app.use(favicon(__dirname + '/public/img/hands4_SKG_icon.ico'));
-
-
-server.connection({
-  host: (process.env.HOST || 'localhost'),
-  port: (process.env.PORT || 3000)
-});
+app.listen(port);
