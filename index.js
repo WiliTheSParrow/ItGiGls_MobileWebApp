4cff938f6181df7c0cb1f475965019505c1b1726
WiliTheSparrow <w.julia91@gmail.com>
2019-08-08 12:06:21 +0200

    how about this

--- index.js
+++ index.js
@@ -15,12 +15,6 @@ app.use(express('public'));
 app.use(favicon(__dirname + '/public/img/hands4_SKG_icon.ico'));
 
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname + '/public/index.html'));
-});
-
-
-
 server.connection({
   host: (process.env.HOST || 'localhost'),
   port: (process.env.PORT || 3000)
