b8559652c2f707c71d86cfc4cfa5926bfe6277c1
WiliTheSparrow <w.julia91@gmail.com>
2019-08-08 14:58:06 +0200

    added Vue.js, scroll down button, js with json

---
+++ public/js/main.js
@@ -0,0 +1,32 @@
+var app = new Vue({
+    el: "#app",
+
+    data: {
+        band: [],
+        date: [],
+        where: []
+    },
+
+    created: function(){},
+
+    methods: {},
+
+});
+
+//Scroll To Top Button:
+$(document).ready(function () {
+
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 40) {
+            $('#topBtn').fadeIn();
+        } else {
+            $('#topBtn').fadeOut();
+        }
+    });
+
+    $("#topBtn").click(function () {
+        $('html , body').animate({
+            scrollTop: 0
+        }, 800);
+    });
+});
