4fc3bdb90e403962042d930c115ba1f058a81ab0
WiliTheSparrow <w.julia91@gmail.com>
2019-08-08 11:47:29 +0200

    lets try this

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 const path = require("path");
+
 var express = require("express");
+
+var favicon = require('serve-favicon');
+
+var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
 var app = express();
+app.use(express.static(__dirname+''));
+app.use(express.static(__dirname));
+
+app.use(express.static('src'));
+app.use(express('public'));
+app.use(favicon(__dirname + '/public/img/hands4_SKG_icon.ico'));
+
 var port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname + '/public/index.html'));
 });
 
-app.use(express.static(__dirname));
+
 
 app.listen(port);
