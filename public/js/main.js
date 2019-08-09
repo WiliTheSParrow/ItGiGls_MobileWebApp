bbc0c613965c8fedd768cefbd8975b99023a6473
WiliTheSparrow <w.julia91@gmail.com>
2019-08-09 14:25:03 +0200

    added vue attribute

--- public/js/main.js
+++ public/js/main.js
@@ -1,32 +1,52 @@
 var gigInfo = data.concerts;
 
+console.log(gigInfo);
+
+
 new Vue({
     el: "#app",
 
-    data: {
-        band: [],
-        date: [],
-        where: []
+    data() {
+        return{
+            data: gigInfo
+        }
     },
 
-    
+    created: function(){
+        this.gigBands()
+    },
+
+    computed: {
+        gigBands(){
+            return this.data.map(dataSet => dataSet[0])
+            console.log("hi")
+        }
+
+       
+    }
+
+    /* created: function(){
+        this.getData();
+    },
 
     methods: {
 
         getData: function(){
-            for(var i = 0; i < this.gigInfo.length; i++){
-                this.band.push(this.gigInfo[i].band);
-                return this.band
+            for(var i = 0; i < gigInfo.length; i++){
+                this.band.push(gigInfo.band[i]);
+                
 
             };
-
-            console.log(this.getData());
+            return this.band;
+            console.log(this.band);
 
         }
 
-    },
+    }, */
 
 });
+
+
 
 //Scroll To Top Button:
 $(document).ready(function () {
