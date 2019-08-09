393054b94e9986ec9640f1ef0986c26d6f935d38
WiliTheSparrow <w.julia91@gmail.com>
2019-08-09 16:11:30 +0200

    X_x

--- public/js/events.js
+++ public/js/events.js
@@ -1,6 +1,6 @@
 var data = {
     "concerts": [
-        {
+      {
         "band": "Static-x",
         "address": "Noordersingel 28/30",
         "place": "Antwerpen",
@@ -10,9 +10,9 @@ var data = {
         "date": "2019-10-06",
         "facebookEvent": "none",
         "ticketPrice": "€31",
-        "ticketBuy": "https://www.eventbrite.be/e/tickets-static-x-59382210791?aff=eprofsaved",
-    }, 
-    {
+        "ticketBuy": "https://www.eventbrite.be/e/tickets-static-x-59382210791?aff=eprofsaved"
+      },
+      {
         "band": "Eluveitie, Lacuna Coil, Infected Rain",
         "address": "Vredenburgkade 11",
         "place": "Utrecht",
@@ -22,9 +22,9 @@ var data = {
         "date": "2019-11-17",
         "facebookEvent": "https://www.facebook.com/events/972543752916280/",
         "ticketPrice": "€33,10",
-        "ticketBuy": "https://www.tivolivredenburg.nl/agenda/eluveitie-17-11-2019/",
-    }, 
-    {
+        "ticketBuy": "https://www.tivolivredenburg.nl/agenda/eluveitie-17-11-2019/"
+      },
+      {
         "band": "Motionless In White",
         "address": "Dommelstraat 2",
         "place": "Eindhoven",
@@ -34,9 +34,9 @@ var data = {
         "date": "2019-11-22",
         "facebookEvent": "none",
         "ticketPrice": "€23,50",
-        "ticketBuy": "https://www.ticketmaster.nl/event/242209?CAMEFROM=EFFENAAR&brand=nl_effenaar",
-    }, 
-    {
+        "ticketBuy": "https://www.ticketmaster.nl/event/242209?CAMEFROM=EFFENAAR&brand=nl_effenaar"
+      },
+      {
         "band": "Northlane",
         "address": "Zijlsingel 2",
         "place": "Haarlem",
@@ -46,8 +46,7 @@ var data = {
         "date": "2019-12-03",
         "facebookEvent": "https://www.facebook.com/events/283118572634471/",
         "ticketPrice": "€22,50",
-        "ticketBuy": "https://www.ticketmaster.nl/event/241851?brand=nl_patronaat&fbclid=IwAR0-LcKAlryZ-g9bJqBT9_-PKgJY21qkPsCRT-wRqJs9yI58Ax3Hr5ywQWU",
-    },
-
-
-]}
+        "ticketBuy": "https://www.ticketmaster.nl/event/241851?brand=nl_patronaat&fbclid=IwAR0-LcKAlryZ-g9bJqBT9_-PKgJY21qkPsCRT-wRqJs9yI58Ax3Hr5ywQWU"
+      }
+    ]
+  }
