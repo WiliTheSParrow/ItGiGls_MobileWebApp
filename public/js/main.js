568755f8b58de19df305ba78ea538c052b9042a8
WiliTheSparrow <w.julia91@gmail.com>
2019-08-15 15:23:08 +0200

    added GM, but it is not working

--- public/js/main.js
+++ public/js/main.js
@@ -41,3 +41,14 @@ $(document).ready(function () {
         }, 800);
     });
 });
+
+//Google Map:
+function initMap() {
+    // The location of Uluru
+    var uluru = {lat: -25.344, lng: 131.036};
+    // The map, centered at Uluru
+    var map = new google.maps.Map(
+        document.getElementById('map'), {zoom: 4, center: uluru});
+    // The marker, positioned at Uluru
+    var marker = new google.maps.Marker({position: uluru, map: map});
+  }
