856bd6f4b58d240e5dc2f0fdabdf9a9134e82731
WiliTheSparrow <w.julia91@gmail.com>
2019-08-15 16:02:05 +0200

    suffered a lil w GM, added collapsible, but doesn wanna work

--- public/js/main.js
+++ public/js/main.js
@@ -4,7 +4,7 @@ var app = new Vue({
     el: "#app",
 
     data: {
-        dataForTable:[],
+        dataForTable: [],
     },
 
     created: function () {
@@ -41,14 +41,39 @@ $(document).ready(function () {
         }, 800);
     });
 });
-
+/* 
 //Google Map:
 function initMap() {
     // The location of Uluru
-    var uluru = {lat: -25.344, lng: 131.036};
+    var uluru = {
+        lat: -25.344,
+        lng: 131.036
+    };
     // The map, centered at Uluru
     var map = new google.maps.Map(
-        document.getElementById('map'), {zoom: 4, center: uluru});
+        document.getElementById('map'), {
+            zoom: 4,
+            center: uluru
+        });
     // The marker, positioned at Uluru
-    var marker = new google.maps.Marker({position: uluru, map: map});
-  }
+    var marker = new google.maps.Marker({
+        position: uluru,
+        map: map
+    });
+} */
+
+//Collapsible:
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+        var content = this.nextElementSibling;
+        if (content.style.display === "block") {
+            content.style.display = "none";
+        } else {
+            content.style.display = "block";
+        }
+    });
+}
