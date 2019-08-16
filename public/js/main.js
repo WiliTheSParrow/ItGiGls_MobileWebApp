1a3c875092ee051c12fe1b7e4e2e55f8b1ccdb15
WiliTheSparrow <w.julia91@gmail.com>
2019-08-16 15:30:10 +0200

    still nothing works

--- public/js/main.js
+++ public/js/main.js
@@ -18,6 +18,10 @@ var app = new Vue({
             for (var i = 0; i < gigInfo.length; i++) {
                 this.dataForTable.push(gigInfo[i]);
             }
+        },
+
+        collapse: function(){
+            
         }
     }
 });
@@ -61,3 +65,20 @@ function initMap() {
         map: map
     });
 } */
+
+
+//Collapsible:
+var coll = document.getElementsByClassName("collapsible");
+
+
+for (var i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "flex") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "flex";
+    }
+  });
+}
