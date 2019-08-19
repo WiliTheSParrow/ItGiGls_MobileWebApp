5b41703ce305d64955827fbf603de4a461cfcf2c
WiliTheSparrow <w.julia91@gmail.com>
2019-08-19 13:15:01 +0200

    fixed collapsible layout

--- public/js/main.js
+++ public/js/main.js
@@ -74,10 +74,10 @@ for (var i = 0; i < coll.length; i++) {
   coll[i].addEventListener("click", function() {
     this.classList.toggle("active");
     var content = this.nextElementSibling;
-    if (content.style.display === "flex") {
+    if (content.style.display === "block") {
       content.style.display = "none";
     } else {
-      content.style.display = "flex";
+      content.style.display = "block";
     }
   });
 }
