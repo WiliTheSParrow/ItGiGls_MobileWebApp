08e6c5d930113404793b8ddd4b8029ca0137541a
WiliTheSparrow <w.julia91@gmail.com>
2019-08-20 12:02:18 +0200

    removed GM, regarding charging issues

--- public/js/main.js
+++ public/js/main.js
@@ -47,28 +47,6 @@ $(document).ready(function () {
 });
 
 
- 
-//Google Map:
-function initMap() {
-    // The location of Uluru
-    var uluru = {
-        lat: -25.344,
-        lng: 131.036
-    };
-    // The map, centered at Uluru
-    var map = new google.maps.Map(
-        document.getElementById('map'), {
-            zoom: 4,
-            center: uluru
-        });
-    // The marker, positioned at Uluru
-    var marker = new google.maps.Marker({
-        position: uluru,
-        map: map
-    });
-};
-
-
 //Collapsible:
 var coll = document.getElementsByClassName("collapsible");
 
