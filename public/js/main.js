c8ab358c1da27fb89d8620f4863be7d373977083
WiliTheSparrow <w.julia91@gmail.com>
2019-09-16 15:00:39 +0200

    cant get this right

--- public/js/main.js
+++ public/js/main.js
@@ -55,17 +55,26 @@ $(document).ready(function () {
 
 
 //Collapsible:
-var coll = document.getElementsByClassName("collapsible");
+ var coll = document.getElementsByClassName("collapsible");
 
 
-for (var i = 0; i < coll.length; i++) {
+ for (var i = 0; i < coll.length; i++) {
+
+  
+    
     coll[i].addEventListener("click", function () {
         this.classList.toggle("active");
+        
         var content = this.nextElementSibling;
         if (content.style.maxHeight) {
             content.style.maxHeight = null;
+            
         } else {
             content.style.maxHeight = content.scrollHeight + "px";
         }
     });
-};
+
+
+}; 
+
+
