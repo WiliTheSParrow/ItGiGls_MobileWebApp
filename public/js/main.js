7a4a8062fbe3dd3f5f0f768bb73ebad5706ce086
WiliTheSparrow <w.julia91@gmail.com>
2019-09-23 15:23:03 +0200

    again collapsible

--- public/js/main.js
+++ public/js/main.js
@@ -130,11 +130,14 @@ $(document).ready(function () {
 
 //Collapsible:
 var coll = document.getElementsByClassName("collapsible");
+
 for (var i = 0; i < coll.length; i++) {
     coll[i].addEventListener("click", function () {
+        
         this.classList.toggle("active");
         //content:
         var content = this.nextElementSibling;
+
         if (content.style.maxHeight) {
             content.style.maxHeight = null;
         } else {
