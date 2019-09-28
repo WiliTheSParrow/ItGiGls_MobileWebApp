4e4ba749ec4bee9be09a297a034c771ec78aaae9
WiliTheSparrow <w.julia91@gmail.com>
2019-09-28 21:43:17 +0200

    still not working T_T

--- public/js/main.js
+++ public/js/main.js
@@ -153,22 +153,25 @@ $(document).ready(function () {
 
 $(document).ready(function () {
     function close_accordion_section() {
-        $('.accordion .accordion-section-title').removeClass('.active');
-        $('.accordion .accordion-section-content').slideUp(300).removeClass('.open');
+        $('.accordion .accordion-section-title').removeClass('active');
+        $('.accordion .accordion-section-content').slideUp(300).removeClass('open');
+        
     }
 
     $('.accordion-section-title').click(function (e) {
 
         var currentAttrValue = $(this).attr('href');
+        
 
-        if ($(e.target).is('.active')) {
+        if ($(e.target).is('active')) {
             close_accordion_section();
+           
         } else {
             close_accordion_section();
 
-            $(this).addClass('.active');
+            $(this).addClass('active');
 
-            $('.accordion ' + currentAttrValue).slideDown(300).addClass('.open');
+            $('.accordion ' + currentAttrValue).slideDown(300).addClass('open');
         }
 
         e.preventDefault();
