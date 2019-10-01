8ba793e6fa86711970efb681aeefd2eae107f73f
WiliTheSparrow <w.julia91@gmail.com>
2019-10-01 15:48:34 +0200

    Sample code is running proprely

--- public/js/main.js
+++ public/js/main.js
@@ -206,3 +206,30 @@ $(document).ready(function () {
         e.preventDefault();
     });
 }); */
+
+// ACCORDION TEST START
+$(document).ready(function () {
+    function close_accordion_section() {
+        $('.accordion .accordion-section-title').removeClass('active');
+        $('.accordion .accordion-section-content').slideUp(300).removeClass('open');
+    }
+
+    $('.accordion-section-title').click(function (e) {
+        // Grab current anchor value
+        var currentAttrValue = $(this).attr('href');
+
+        if ($(e.target).is('.active')) {
+            close_accordion_section();
+        } else {
+            close_accordion_section();
+
+            // Add active class to section title
+            $(this).addClass('active');
+            // Open up the hidden content panel
+            $('.accordion ' + currentAttrValue).slideDown(300).addClass('open');
+        }
+
+        e.preventDefault();
+    });
+});
+// ACCORDION TEST END
